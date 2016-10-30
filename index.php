5f51b94c4aff5349893aa178cc5381835decd918
nikcani <niklascanisius@me.com>
2016-10-30 19:11:59 +0100

    altorouter, composer and kint added

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require 'vendor/autoload.php';
+header("Content-Type: text/html");
+
+$router = new AltoRouter();
+$router->setBasePath('apps/vorlesetag/');
+/* Setup the URL routing. This is production ready. */
+
+// Main routes that non-customers see
+$router->map('GET','/', 'home.php', 'home');
+$router->map('GET','home', function (){
+    d("test");
+}, 'home-home');
+
+
+/* Match the current request */
+$match = $router->match();
+if($match) {
+    require $match['target'];
+}
+else {
+    header("HTTP/1.0 404 Not Found");
+    echo '404.html';
+}
