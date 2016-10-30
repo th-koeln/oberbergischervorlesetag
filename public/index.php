407247580569d18f4caae5102bc1bd81c20aaa4d
nikcani <niklascanisius@me.com>
2016-10-30 20:13:34 +0100

    removed altorouter

--- public/index.php
+++ public/index.php
@@ -1,24 +1,5 @@
 <?php
 declare(strict_types = 1);
-require 'vendor/autoload.php';
-header("Content-Type: text/html");
+require "../vendor/autoload.php";
 
-$router = new AltoRouter();
-$router->setBasePath('apps/vorlesetag/');
-/* Setup the URL routing. This is production ready. */
-
-// Main routes that non-customers see
-$router->map('GET', '/', 'home.php', 'home');
-$router->map('GET', 'home', function () {
-    d("test");
-}, 'home-home');
-
-
-/* Match the current request */
-$match = $router->match();
-if ($match) {
-    require $match['target'];
-} else {
-    header("HTTP/1.0 404 Not Found");
-    echo '404.html';
-}
+phpinfo();
